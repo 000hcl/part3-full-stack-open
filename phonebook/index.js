cbfef26954892f1e77fb03e7cb4a5fcbce445138
Natalie Johansson <mnrjohansson@gmail.com>
2022-08-31 14:28:28 +0300

    3.4

--- phonebook/index.js
+++ phonebook/index.js
@@ -46,6 +46,13 @@ app.get('/api/persons/:id', (request, response)=>{
   
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(p => p.id !== id)
+
+  response.status(204).end()
+})
+
 app.get('/info', (request, response) => {
     const date = new Date();
     message = `Phonebook has info for ${persons.length} people <br> ${date}`
