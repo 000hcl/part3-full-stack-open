60d73ed3f2607bc7af9f8d86da892cdee98e8e04
Natalie Johansson <mnrjohansson@gmail.com>
2022-09-03 13:46:12 +0300

    finish 3.13

--- phonebook/index.js
+++ phonebook/index.js
@@ -56,14 +56,19 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response)=>{
-  const id = Number(request.params.id)
-  const person = persons.find(p => p.id === id)
+  Person.findById(request.params.id)
+  .then(person => {
+    if (person) {
+      response.json(person)
 
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+    } else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => {
+    console.log(error);
+    response.status(400).send({error: 'malformatted id'})
+  })
   
 })
 
